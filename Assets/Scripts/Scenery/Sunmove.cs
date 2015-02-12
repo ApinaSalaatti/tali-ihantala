c1cfd4665d0231b8f4043858499f3dcd62757020
voinekku <talvimies@hotmail.com>
2015-02-13 00:43:24 +0200

    new graphical assets and minor tweaks to shooting values

---
+++ Assets/Scripts/Scenery/Sunmove.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sunmove : MonoBehaviour 
+{
+	public float Windspeed;
+	public float resetpoint;
+
+
+	void Update() 
+	{
+		if (transform.position.x < resetpoint) {
+			transform.Translate (Vector3.right * Time.deltaTime*Windspeed);
+		} 
+
+		else 
+		{
+			transform.Translate (-Vector3.right * 2f *resetpoint);
+		}
+	}
+}
