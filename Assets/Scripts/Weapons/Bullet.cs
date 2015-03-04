a3e1ef04b187c52fdbadd8b01152cc2f2cb263af
voinekku <talvimies@hotmail.com>
2015-03-04 18:53:55 +0200

    bullet effects

--- Assets/Scripts/Weapons/Bullet.cs
+++ Assets/Scripts/Weapons/Bullet.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 public class Bullet : MonoBehaviour {
 	public float damage;
+	public GameObject smoke;
+	public GameObject flash;
+	public GameObject bloodspill;
+	public GameObject bloodcloud;
 
 	// Use this for initialization
 	void Start () {
@@ -19,12 +23,25 @@ public class Bullet : MonoBehaviour {
 		BulletEffect[] effects = GetComponents<BulletEffect>();
 		foreach(BulletEffect e in effects) {
 			e.Affect(go);
+
+
 		}
 
 		Destroy(gameObject);
 	}
 
 	void OnCollisionEnter(Collision col) {
-		HitObject(col.collider.gameObject);
+
+			HitObject (col.collider.gameObject);
+
+		//tuo flashi pitäs saada syntyyn luodin suunan vastaiseen suuntaan ja veriroiskeet samaan suuntaan... mitenkähän se onnistuis?
+		//nää elottomaan:
+		Instantiate (smoke, transform.position, transform.rotation);
+		Instantiate (flash, transform.position, transform.rotation);
+
+		//nää kaikkeen elävään:
+		//Instantiate (bloodcloud, transform.position, transform.rotation);
+		//Instantiate (bloodspill, transform.position, transform.rotation);
+		
 	}
 }
