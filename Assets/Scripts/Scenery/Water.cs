749e7b2b7b784480fe53b109b8de46ed2f5cd149
voinekku <talvimies@hotmail.com>
2015-03-23 15:03:25 +0200

    fixed tree colliders and added water movement

---
+++ Assets/Scripts/Scenery/Water.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Water : MonoBehaviour {
+	public float scrollSpeed = 0.05F;
+	public Renderer rend;
+
+
+	void Start() {
+		rend = GetComponent<Renderer>();
+	}
+	void Update() {
+		float offset = Time.time * scrollSpeed;
+		rend.material.SetTextureOffset("_MainTex", new Vector2(offset, 15));
+		rend.material.SetTextureOffset("_MainTex", new Vector2(15, offset));
+	}
+}
